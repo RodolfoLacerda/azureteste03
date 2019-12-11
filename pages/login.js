00b216a4fa5574baebd431d9cb91344bee52d3e7
rodolfo.lacerda <rodolfo.lacerda@memt.com.br>
2019-12-11 14:05:32 -0300

    first commit

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    url: 'https://smartenergy-qa.memt.com.br/',
+    elements: {
+        menu: '.MuiIconButton-edgeStart',
+        acessarMenu: '.MuiIconButton-colorSecondary',
+        menuContratos: '.MuiTypography-root:nth-child(3) .MuiTypography-root',
+        adicionarContrato: '.MuiButton-sizeSmall',
+        pontoConexao: '#id_connectionpoint',
+        tipoContrato: '.MuiOutlinedInput-root',
+        
+        selecionarTIpoContrato: '//li[contains(.,"Distribuição")]'
+        
+      
+    }
+}
